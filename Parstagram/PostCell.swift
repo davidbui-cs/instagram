305ac0841da17d93143985e4023a57642e8a6ddf
David Bui <davidbui@Davids-MacBook-Pro.local>
2019-02-17 19:13:24 -0800

    First commit

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by David Bui on 2/17/19.
+//  Copyright © 2019 David Bui. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
