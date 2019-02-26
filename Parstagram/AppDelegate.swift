e6d7be47398d47f79a5475c65288b02b1445a27a
david bui <davidbui1998@gmail.com>
2019-02-25 17:06:17 -0800

    Updated

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -23,6 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://parseinstagram-david.herokuapp.com/parse"
             })
         )
+        if  PFUser.current() != nil{
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        }
         return true
     }
 
